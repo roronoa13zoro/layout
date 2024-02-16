425ec63e7047875283f28cc7af942c6f07a31f30
roronoa13zoro <123082798+roronoa13zoro@users.noreply.github.com>
2024-02-16 10:26:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index1.html')
 
 @app.route("/index1")
 def index1():
@@ -13,14 +13,6 @@ def index1():
 @app.route("/index2")
 def index2():
 	return render_template('index2.html')
-
-@app.route("/index3")
-def index3():
-	return render_template('index3.html')
-
-@app.route("/index4")
-def index4():
-	return render_template('index4.html')
 
 if __name__ == '__main__':
     app.run(port=8000,host="0.0.0.0")
